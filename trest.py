05c51f9d6f249526d697a3aadc758456c21a8536
julesgtz <113105305+julesgtz@users.noreply.github.com>
2023-02-09 08:37:28 +0100

    Update trest.py

--- trest.py
+++ trest.py
@@ -1,7 +1,7 @@
 import re
 
 def check_machine_name(url):
-    pattern = re.compile('^www\.[\W]+\.[\W]{2,3}$')
+    pattern = re.compile('^www\.[\w]+\.[\w]{2,3}$')
     match = pattern.match(url)
     if match:
         return True
